36ac32214f5eb6d2f876cdb0fc69600a08c5622a
DanielOliveiraLafeta <daniel.oliveira.lafeta@gmail.com>
2021-12-20 10:29:08 -0300

    add snake direction and infinite loop snake screen

--- script.js
+++ script.js
@@ -20,7 +20,24 @@ function createSnake() {
     }
 }
 
+//evento para detectar movimentos na tela
+document.addEventListener("keydown", update) //detecta ações de clique na tela
+//função para direcionar a cobrinha de acordo com a tecla apertada
+//evitando que mude na direção contrária
+function update(event) {
+    if(event.keyCode == 37 && direction != "right") direction = "left"
+    if(event.keyCode == 38 && direction != "down") direction = "up"
+    if(event.keyCode == 39 && direction != "left") direction = "right" 
+    if(event.keyCode == 40 && direction != "up") direction = "down"
+}
+
 function startGame() {
+    //condições que evitam que a cobrinha atravesse as paredes(a tela do jogo)
+    if(snake[0].x > 15*box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16*box;
+    if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16*box;
+
     createBG()  
     createSnake()
 
@@ -30,18 +47,17 @@ function startGame() {
     //coordenadas da cobrinha
     if(direction == "right") snakeX += box
     if(direction == "left") snakeX -= box
-    if(direction == "up") snakeY += box
-    if(direction == "down") snakeY -= box
+    if(direction == "up") snakeY -= box
+    if(direction == "down") snakeY += box
 
     //retira o último elemento do array(da cobrinha)
     snake.pop()
 
-    //adiciona um elemento na frente do array(da cobrinha)
+    //adiciona elemento na primeira posição do array(da cobrinha)
     let newHead = {
-        x: snakeX,
+        x:snakeX,
         y:snakeY
     }
-
     snake.unshift(newHead)
 }
 
