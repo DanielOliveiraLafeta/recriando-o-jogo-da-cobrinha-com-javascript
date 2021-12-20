8cb908ba1a8a4b0b2b9c15df3ce9e1e3281424bc
DanielOliveiraLafeta <daniel.oliveira.lafeta@gmail.com>
2021-12-20 09:41:15 -0300

    add snake move function

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right"
 
 function createBG() {
     context.fillStyle = "lightgreen" //cor do fundo(canvas) 
@@ -19,6 +20,31 @@ function createSnake() {
     }
 }
 
+function startGame() {
+    createBG()  
+    createSnake()
 
-createBG()
-createSnake()
+    let snakeX = snake[0].x //posição da cobrinha no eixo de x
+    let snakeY = snake[0].y // posição da cobrinha no eixo de y
+
+    //coordenadas da cobrinha
+    if(direction == "right") snakeX += box
+    if(direction == "left") snakeX -= box
+    if(direction == "up") snakeY += box
+    if(direction == "down") snakeY -= box
+
+    //retira o último elemento do array(da cobrinha)
+    snake.pop()
+
+    //adiciona um elemento na frente do array(da cobrinha)
+    let newHead = {
+        x: snakeX,
+        y:snakeY
+    }
+
+    snake.unshift(newHead)
+}
+
+
+let game = setInterval(startGame, 100) // renova a função startGame a cada 100 milisegundos
+
